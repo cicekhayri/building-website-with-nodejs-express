f9944e9f61399a3dbff9f86c34c89c71010d2751
Hayri Cicek <hayri.cicek@hiq.se>
2023-11-07 11:52:42 +0100

    Adds artwork partial and dynamically load user artwork

--- routes/speakers.js
+++ routes/speakers.js
@@ -5,13 +5,20 @@ module.exports = (params) => {
   const { speakersService } = params;
 
   router.get('/', async (request, response) => {
+    const artwork = await speakersService.getAllArtwork();
     const speakers = await speakersService.getList();
-    response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers });
+    response.render('layout', { pageTitle: 'Speakers', template: 'speakers', speakers, artwork });
   });
 
   router.get('/:shortname', async (request, response) => {
     const speaker = await speakersService.getSpeaker(request.params.shortname);
-    response.render('layout', { pageTitle: 'Speakers', template: 'speakers-detail', speaker });
+    const artwork = await speakersService.getArtworkForSpeaker(request.params.shortname);
+    response.render('layout', {
+      pageTitle: 'Speakers',
+      template: 'speakers-detail',
+      speaker,
+      artwork,
+    });
   });
 
   return router;
