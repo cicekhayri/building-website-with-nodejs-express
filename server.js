1b115a862794e5d32b199b45f23127ef34ee7367
Hayri Cicek <hayri.cicek@hiq.se>
2023-11-07 12:16:51 +0100

    Adds error page

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const cookieSession = require('cookie-session');
 
+const createError = require('http-errors');
+
 const FeedbackService = require('./services/FeedbackService');
 const SpeakerService = require('./services/SpeakerService');
 
@@ -47,6 +49,19 @@ app.use(
     speakersService,
   })
 );
+
+app.use((request, response, next) => {
+  return next(createError(404, 'File not found'));
+});
+
+app.use((err, request, response, next) => {
+  response.locals.message = err.message;
+  console.error(err);
+  const status = err.status || 500;
+  response.locals.status = status;
+  response.status(status);
+  response.render('error');
+});
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
