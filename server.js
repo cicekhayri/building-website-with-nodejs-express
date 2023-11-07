a42332710f45c57c956b1916b975f0a8a032ae1d
Hayri Cicek <hayri.cicek@hiq.se>
2023-11-07 09:34:23 +0100

    Adds middleware for serving static files

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "./static")));
+
 app.get("/", (request, response) => {
     response.sendFile(path.join(__dirname, "./static/index.html"));
+});
+
+app.get("/speakers", (request, response) => {
+    response.sendFile(path.join(__dirname, "./static/speakers.html"));
 });
 
 app.listen(port, () => {
